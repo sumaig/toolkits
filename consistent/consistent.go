7b96e6a824427bfd10551bcd61e87ec3f77476c7
Sumai <jack.gao.ch@gmail.com>
2018-04-19 16:28:27 +0800

    support remove node

--- consistent/consistent.go
+++ consistent/consistent.go
@@ -4,6 +4,7 @@ import (
 	"hash/crc32"
 	"sort"
 	"strconv"
+	"sync"
 )
 
 type Hash func(data []byte) uint32
@@ -11,8 +12,9 @@ type Hash func(data []byte) uint32
 type Map struct {
 	hash     Hash
 	replicas int
-	keys     []int // Sorted
+	nodes    []int // Sorted
 	hashMap  map[int]string
+	sync.RWMutex
 }
 
 func New(replicas int, fn Hash) *Map {
@@ -29,23 +31,49 @@ func New(replicas int, fn Hash) *Map {
 
 // Returns true if there are no items available.
 func (m *Map) IsEmpty() bool {
-	return len(m.keys) == 0
+	return len(m.nodes) == 0
 }
 
-// Adds some keys to the hash.
-func (m *Map) Add(keys ...string) {
-	for _, key := range keys {
+// Adds some nodes to the hash.
+func (m *Map) Add(nodes ...string) {
+	m.Lock()
+	defer m.Unlock()
+	for _, node := range nodes {
 		for i := 0; i < m.replicas; i++ {
-			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
-			m.keys = append(m.keys, hash)
-			m.hashMap[hash] = key
+			hash := int(m.hash([]byte(strconv.Itoa(i) + node)))
+			m.nodes = append(m.nodes, hash)
+			m.hashMap[hash] = node
 		}
 	}
-	sort.Ints(m.keys)
+	sort.Ints(m.nodes)
+}
+
+// Remove some nodes from the hash
+func (m *Map) Remove(nodes ...string) {
+	m.Lock()
+	defer m.Unlock()
+	for _, node := range nodes {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + node)))
+			delete(m.hashMap, hash)
+			m.deleteSlice(hash)
+		}
+	}
+}
+
+// delete element from nodes slice
+func (m *Map) deleteSlice(hash int) {
+	for i := 0; i < len(m.nodes); i++ {
+		if m.nodes[i] == hash {
+			m.nodes = append(m.nodes[:i], m.nodes[i+1:]...)
+		}
+	}
 }
 
 // Gets the closest item in the hash to the provided key.
 func (m *Map) Get(key string) string {
+	m.RLock()
+	defer m.RUnlock()
 	if m.IsEmpty() {
 		return ""
 	}
@@ -53,12 +81,12 @@ func (m *Map) Get(key string) string {
 	hash := int(m.hash([]byte(key)))
 
 	// Binary search for appropriate replica.
-	idx := sort.Search(len(m.keys), func(i int) bool { return m.keys[i] >= hash })
+	idx := sort.Search(len(m.nodes), func(i int) bool { return m.nodes[i] >= hash })
 
 	// Means we have cycled back to the first replica.
-	if idx == len(m.keys) {
+	if idx == len(m.nodes) {
 		idx = 0
 	}
 
-	return m.hashMap[m.keys[idx]]
+	return m.hashMap[m.nodes[idx]]
 }
